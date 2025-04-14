5fe6b261dc055b33bc1d163e52f9df2aa63ebc59
basilioarth <basilio.arth@gmail.com>
2025-04-14 17:26:04 -0300

    feat: Criando o schema base do banco de dados

---
+++ server/src/infra/db/schemas/urls.ts
@@ -0,0 +1,14 @@
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { uuidv7 } from 'uuidv7'
+
+export const urls = pgTable('urls', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
+  originalUrl: text('original_url').notNull(),
+  shortenedUrl: text('shortened_url').notNull().unique(),
+  accessCount: integer('access_count').default(0),
+  createdAt: timestamp('created_at')
+    .defaultNow()
+    .notNull(),
+});
