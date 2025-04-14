91759e4e08c7b364b44dc36f66c7e4f54cdb4a95
basilioarth <basilio.arth@gmail.com>
2025-04-14 16:51:55 -0300

    ops: Configurando o arquivo principal da aplicação backend com a adição de um validador, um serializador, uma tratativa para erros de validação e o Scalar para fazer a documentação das funcionalidades da API

---
+++ server/src/infra/http/server.ts
@@ -0,0 +1,57 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { 
+    serializerCompiler, 
+    validatorCompiler, 
+    hasZodFastifySchemaValidationErrors,
+    jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import scalarUI  from "@scalar/fastify-api-reference";
+
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.setErrorHandler((error, request, reply) => {
+    if (hasZodFastifySchemaValidationErrors(error)) {
+        return reply.status(400).send({
+            message: 'Validation error.',
+            issues: error.validation,
+        })
+    }
+
+    console.error(error)
+
+    return reply.status(500).send({
+        message: 'Internal server error.',
+    }) 
+});
+
+app.register(fastifyCors, {
+    origin: '*',
+});
+
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: "Documentação da API de encurtamento de URLs",
+            version: "1.0.0"
+        },
+    },
+    transform: jsonSchemaTransform
+});
+
+app.get('/openapi.json', () => app.swagger());
+
+app.register(scalarUI, {
+    routePrefix: '/docs',
+    configuration: {
+        layout: 'modern',
+    }
+});
+
+app.listen({ port: 3333 }).then(() => {
+    console.log(`HTTP server running! Go to http://localhost:3333/docs to view the documentation`)
+});
