c80175941d42fd724d9af79870b8d0e795a5cad5
basilioarth <basilio.arth@gmail.com>
2025-04-26 18:45:51 -0300

    ops: Utilizando CSS Modules na aplicação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,18 @@
+import styles from './App.module.css';
+import logo from './assets/Logo.svg';
+
+import { NewLink } from './components/NewLink';
+
 export function App() {
   return (
-    <div>
-      <h1>Brev.ly</h1>
-    </div>
+    <main className={styles.container}>
+      <div className={styles.header}>
+        <img src={logo} alt="Logo do Brev.ly" />
+      </div>
+      <div className={styles.content}>
+        <NewLink />
+        <p>My links</p>
+      </div>
+    </main>
   )
 }
