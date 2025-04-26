3a6bb482dc90ef0ed922df57570b44eb254ebc14
basilioarth <basilio.arth@gmail.com>
2025-04-26 18:45:15 -0300

    ops: Adicionando o provider do ChakraUI na aplicação

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,13 @@
 import './global.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={defaultSystem}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
