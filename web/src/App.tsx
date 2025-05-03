aad75db61559f6089949fa24c2e7ac3694f56eed
basilioarth <basilio.arth@gmail.com>
2025-05-03 10:16:39 -0300

    feat: Instalando e utilizando react-router-dom & Reorganizando a aplicação com a criação das páginas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,14 @@
-import styles from './App.module.css';
-import logo from './assets/Logo.svg';
-
-import { NewLink } from './components/NewLink';
-import { MyLinks } from './components/MyLinks';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Links } from "./pages/Links";
+import { Redirect } from "./pages/Redirect";
 
 export function App() {
   return (
-    <main className={styles.container}>
-      <div className={styles.header}>
-        <img src={logo} alt="Logo do Brev.ly" />
-      </div>
-      <div className={styles.content}>
-        <NewLink />
-        <MyLinks />
-      </div>
-    </main>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Links />} />
+        <Route path="*" element={<Redirect />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- web/src/components/Link.tsx
+++ web/src/components/Link.tsx
@@ -3,6 +3,7 @@ import { Copy, Trash } from 'phosphor-react';
 import { deleteLink } from '../http/services/deleteLink';
 import { toaster } from './ui/toaster';
 import { useLinksStore } from '../store/linksStore';
+import { useNavigate } from 'react-router-dom';
 
 import styles from './Link.module.css';
 
@@ -15,6 +16,26 @@ interface LinkProps {
 
 export function Link({ id, originalUrl, shortenedUrl, accessCount }: LinkProps) {
     const { removeLink } = useLinksStore();
+    const navigate = useNavigate();
+
+    const handleClickOnShortenedLink = async () => {
+        navigate(`/${shortenedUrl}`);
+    }
+
+    const handleCopyToClipboard = async () => {
+        try {
+            await navigator.clipboard.writeText(`http://localhost:5173/${shortenedUrl}`);
+            toaster.success({
+                title: "Link copiado para a área de transferência!",
+                type: 'success',
+            });
+          } catch (err: any) {
+            toaster.error({
+                title: "Erro ao copiar o link para a área de transferência",
+                type: 'error',
+            });
+          }
+    }
 
     const handleDeleteLink = async () => {
         try {
@@ -35,13 +56,13 @@ export function Link({ id, originalUrl, shortenedUrl, accessCount }: LinkProps) 
     return (
         <main className={styles.container}>
             <div className={styles.title}>
-                <h1>brev.ly/{shortenedUrl}</h1>
+                <h1 onClick={handleClickOnShortenedLink}>brev.ly/{shortenedUrl}</h1>
                 <h2>{originalUrl}</h2>
             </div>
             <div className={styles.actions}>
                 <p>{accessCount} {accessCount === 1 ? "acesso" : "acessos"}</p>
                 <div className={styles.buttonsContainer}>
-                    <Button className={styles.actionButton}>
+                    <Button className={styles.actionButton} onClick={handleCopyToClipboard}>
                         <Copy className={styles.buttonIcon}/>
                     </Button>
                     <Button className={styles.actionButton} onClick={handleDeleteLink}>
